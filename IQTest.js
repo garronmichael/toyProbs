430d50a16600e0afc2984e3003e9adcb153d5e80
Garron Sanchez <garron.michael@gmail.com>
2015-05-04 11:11:11 -0700

    add solutions to IQTest.js

--- IQTest.js
+++ IQTest.js
@@ -5,3 +5,35 @@ Bob is preparing to pass IQ test. The most frequent task in this test is to find
 ! Keep in mind that your task is to help Bob solve a real IQ test, which means indexes of the elements start from 1 (not 0)
 
 */
+
+// my solution:
+
+function iqTest(numbers){
+  // convert string to an array
+  var nums = numbers.split(' ');
+  var even = [];
+  var odd = [];
+  // determine if which numbers are even and which are odd
+  for(var i = 0; i < nums.length; i++) {
+    if(nums[i] % 2 === 0) {
+      even.push(nums[i]);
+    } else {
+      odd.push(nums[i]);
+    }
+  }
+  // if the length of the sub array is one, return the index + 1 of that value
+  // from the original array
+  if(even.length === 1) {
+    return nums.indexOf(even[0]) + 1;
+  } else if(odd.length === 1) {
+    return nums.indexOf(odd[0]) + 1;
+  }  
+}
+
+// clever solution:
+
+function iqTest(numbers){
+  numbers=numbers.split(" ").map(function(x){return parseInt(x)%2;});
+  if (numbers[0]+numbers[1]+numbers[2]==3 || numbers[0]+numbers[1]+numbers[2]==2) return numbers.indexOf(0)+1;
+  else return numbers.indexOf(1)+1;
+}
