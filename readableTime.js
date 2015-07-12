9f84fe06ba4db4fc3399570db7850523f2e4d03a
Garron Sanchez <garron.michael@gmail.com>
2015-07-11 21:02:24 -0700

    complete readableTime

---
+++ readableTime.js
@@ -0,0 +1,34 @@
+/*
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+
+*/
+
+function humanReadable(seconds) {
+  var HH = Math.floor(seconds / 3600).toString();
+  var MM = (Math.floor(seconds / 60) - 60 * +HH).toString();
+  var SS = Math.floor(seconds % 60).toString();
+  var t = [HH, MM, SS];
+  t.forEach(function(v, i, a) {
+    if(v.length < 2) {
+    a[i] = '0' + v;
+    }
+  });
+  
+  return t.join(':');
+}
+
+// Solution
+
+function humanReadable(seconds) {
+  var pad = function(x) { return (x < 10) ? "0"+x : x; }
+  return pad(parseInt(seconds / (60*60))) + ":" +
+         pad(parseInt(seconds / 60 % 60)) + ":" +
+         pad(seconds % 60)
+}
