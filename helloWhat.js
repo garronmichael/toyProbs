340fae1548ceece63be58235a00e8c7c37fb7ed3
Garron Sanchez <garron.michael@gmail.com>
2015-07-19 14:33:52 -0700

    complete helloWhat and add solution

--- helloWhat.js
+++ helloWhat.js
@@ -31,3 +31,25 @@ var helloWorld = function () {
 };
 
 console.log(helloWorld());
+
+// Solution
+
+var helloWorld = function () {
+  var one = +true
+  var two = one + one
+  var four = two + two
+  var eight = four + four
+  var sixteen = eight + eight
+  var thirtytwo = sixteen + sixteen
+  var sixtyfour = thirtytwo + thirtytwo
+  var sp = thirtytwo
+  var excl = thirtytwo + one
+  var H = sixtyfour + eight
+  var e = sixtyfour + thirtytwo + four + one
+  var l = sixtyfour + thirtytwo + eight + four
+  var o = sixtyfour + thirtytwo + sixteen - one
+  var W = sixtyfour + sixteen + eight - one
+  var r = sixtyfour + thirtytwo + sixteen + two
+  var d = sixtyfour + thirtytwo + four
+  return [H, e, l, l, o, sp, W, o, r, l, d, excl].map(function (c) { return String.fromCharCode(c) }).join([])
+}
