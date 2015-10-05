8b4066ce18f3e4a9ac1a1ff5023330dc2bac2e4e
Garron Sanchez <garron.michael@gmail.com>
2015-10-05 09:30:58 -0700

    initial commit

---
+++ routeBtwnTwoNodes.js
@@ -0,0 +1,16 @@
+/*
+
+A directed graph consistst of a set of nodes and a set of edges that connect it to other nodes:
+
+Node = {
+  value:'a',
+  edges: [node1, node2, ...]
+}
+Given an acyclic graph (a graph with no loops) like the following: (http://en.wikipedia.org/wiki/Directed_graph)
+
+Or a graph with cycles like the following: (http://upload.wikimedia.org/wikipedia/commons/a/a2/Directed.svg)
+
+Find whether there is a route between two nodes of the graph.
+
+*/
+
