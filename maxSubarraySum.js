1b410c56351a0941e8af870b97fadf3d8eb6709f
Garron Sanchez <garron.michael@gmail.com>
2015-10-11 09:33:52 -0700

    add solution

--- maxSubarraySum.js
+++ maxSubarraySum.js
@@ -35,3 +35,15 @@ var maxSequence = function(arr){
 }
 
 console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])) // 6
+
+// Solution
+
+var maxSequence = function(arr){
+  var min = 0, ans = 0, i, sum = 0;
+  for (i = 0; i < arr.length; ++i) {
+    sum += arr[i];
+    min = Math.min(sum, min);
+    ans = Math.max(ans, sum - min);
+  }
+  return ans;
+}
