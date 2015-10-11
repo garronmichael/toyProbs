c1be6362846292146d720a348a04b9e62265d2a3
Garron Sanchez <garron.michael@gmail.com>
2015-10-11 08:55:05 -0700

    start solution

--- maxSubarraySum.js
+++ maxSubarraySum.js
@@ -9,3 +9,10 @@ Easy case is when the list is made up of only positive numbers and the maximum s
 Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
 
 */
+
+var maxSequence = function(arr){
+  if(arr.length) {
+  } else {
+    return 0;
+   }
+}
