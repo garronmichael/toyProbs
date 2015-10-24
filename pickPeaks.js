61f878a3ef7a428f0ccae1ea6660fb76cc8a0599
Garron Sanchez <garron.michael@gmail.com>
2015-10-24 09:27:50 -0700

    add solution

--- pickPeaks.js
+++ pickPeaks.js
@@ -68,3 +68,22 @@ function pickPeaks(arr){
 // console.log(pickPeaks([3,2,3,6,4,1,2,3,2,1,2,3])); // {pos:[3,7],peaks:[6,3]} PASSED
 // console.log(pickPeaks([ 3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1 ])); //  {"pos":[3,7,10],"peaks":[6,3,2]} PASSED
 console.log(pickPeaks([ 2, 1, 3, 2, 2, 2, 2, 1 ])); // {"pos":[2],"peaks":[3]}, instead got: {"pos":[2,3],"peaks":[3,2]}
+
+// Solution
+
+function pickPeaks(arr){
+  var result = {pos: [], peaks: []};
+  if(arr.length > 2) {
+    var pos = -1;
+    for(var i=1; i<arr.length;i++){
+      if(arr[i] > arr[i-1]) {
+        pos = i;
+      } else if(arr[i] < arr[i-1] && pos != -1) {
+        result.pos.push(pos);
+        result.peaks.push(arr[pos]);
+        pos = -1;
+      }
+    }
+  }
+  return result;
+}
