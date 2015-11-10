87de989e6e20ab70533373a467def81ee3f24c22
Garron Sanchez <garronmichael@gmail.com>
2015-11-09 16:11:54 -0800

    almost complete

--- didYouMean.js
+++ didYouMean.js
@@ -36,10 +36,67 @@ there is always exactly one possible solution
 
 */
 
+function countLetters(word) {
+  var obj = {};
+  for(var i = 0; i < word.length; i++) {
+    var char = word[i];
+    var re = new RegExp(char, 'gi');
+    obj[char] = word.match(re).length;
+  }
+  return [obj, word];
+}
+
 function Dictionary(words) {
   this.words = words;
 }
 
 Dictionary.prototype.findMostSimilar = function(term) {
-  // TODO: this is your task ;)
+  // if the term is in the dictionary
+  if(this.words.indexOf(term) > -1) {
+    // return it
+    return term;
+  // otherwise
+  } else {
+    var leastDiff = term.length;
+    var closestTerm;
+    var words = this.words.map(countLetters);
+    var term = countLetters(term);
+    for(var i = 0; i < words.length; i++) {
+      var curDiff = 0;
+      var curWord = words[i];
+      // each letter[value] in term must equal letter[value] in each word in words
+      for(var letter in curWord[0]) {
+        // each addition of a letter[value] to term to match letter[value] in word results in an increment to curDiff
+        if(!term[0][letter] || curWord[0][letter] > term[0][letter]) {
+          term[0][letter] = term[0][letter] || 0;
+          curDiff += curWord[0][letter] - term[0][letter]++;
+          term[0][letter] = curWord[0][letter];
+        }
+        // each removal of a letter results in an increment to curDiff
+        if(curWord[0][letter] < term[0][letter]) {
+          curDiff += term[0][letter] - curWord[0][letter];
+          term[0][letter] = curWord[0][letter];
+        }
+      }
+      // if the curDiff < leastDiff
+      if(curDiff < leastDiff) {
+        // there is a new leastDiff and closestTerm
+        leastDiff = curDiff;
+        closestTerm = curWord[1];
+      }
+    }
+    return closestTerm; 
+  }
+
 }
+
+var fruits = new Dictionary( ['cherry', 'pineapple', 'melon', 'strawberry', 'raspberry'] );
+console.log(fruits.findMostSimilar('strawbery')) // 'strawberry'
+console.log(fruits.findMostSimilar('berry')); // must return "cherry"
+
+things = new Dictionary(['stars', 'mars', 'wars', 'codec', 'codewars']);
+console.log(things.findMostSimilar('coddwars')); // must return "codewars"
+
+languages = new Dictionary(['javascript', 'java', 'ruby', 'php', 'python', 'coffeescript']);
+console.log(languages.findMostSimilar('heaven')); // must return "java"
+console.log(languages.findMostSimilar('javascript')); // must return "javascript" (same words are obviously the most similar ones)
