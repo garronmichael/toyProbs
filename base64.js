a98736ccdd9243931a89d635a6cb7cd42337cd3e
Garron Sanchez <garronmichael@gmail.com>
2015-11-30 10:29:46 -0800

    attempt base64

--- base64.js
+++ base64.js
@@ -13,4 +13,33 @@ You can learn more about Base64 encoding and decoding
 
 */
 
-//Extend the String object with toBase64() and fromBase64() functions
+// Extend the String object with toBase64() and fromBase64() functions
+
+String.prototype.toBase64 = function() {
+  var string = this.valueOf().split(''), 
+      output = [];
+  console.log(string);
+  string.forEach( (v, i, a) => {
+    v = v.charCodeAt(0).toString(2);
+    while(v.length < 8) {
+    v = '0'.concat(v);
+    }
+    a[i] = v;
+  });
+  string = string.join('').split('');
+  while(string.length) {
+    output.push(string.splice(0, 6).join(''));
+  }
+  
+  output.forEach( (v, i, a) => {
+    console.log(v);
+    a[i] = String.fromCharCode(parseInt(v, 2) + 64);
+  });
+  
+  return output.join('');
+  
+}
+
+String.prototype.fromBase64 = function() {
+ var string = this.valueOf();
+}
