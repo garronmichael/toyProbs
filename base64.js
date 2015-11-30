c166c81a477c3f73b213be8ab2e1ce3d66a1b78c
Garron Sanchez <garronmichael@gmail.com>
2015-11-30 10:29:01 -0800

    initial commit

---
+++ base64.js
@@ -0,0 +1,16 @@
+/*
+
+Extend the String object to create a function that converts the value of the String to and from Base64 using the ASCII character set.
+
+Usage:
+
+// should return 'dGhpcyBpcyBhIHN0cmluZyEh'
+'this is a string!!'.toBase64(); 
+
+// should return 'this is a string!!'
+'dGhpcyBpcyBhIHN0cmluZyEh'.fromBase64();
+You can learn more about Base64 encoding and decoding
+
+*/
+
+//Extend the String object with toBase64() and fromBase64() functions
