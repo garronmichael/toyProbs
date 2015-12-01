3691b0fb83cff43423f6552ed39db77be7f6510a
Garron Sanchez <garronmichael@gmail.com>
2015-12-01 15:03:36 -0800

    almost complete

--- base64.js
+++ base64.js
@@ -2,7 +2,7 @@
 
 Extend the String object to create a function that converts the value of the String to and from Base64 using the ASCII character set.
 
-Usage:
+'Usage':
 
 // should return 'dGhpcyBpcyBhIHN0cmluZyEh'
 'this is a string!!'.toBase64(); 
@@ -15,31 +15,207 @@ You can learn more about Base64 encoding and decoding
 
 // Extend the String object with toBase64() and fromBase64() functions
 
+var lib1 = {
+  0: 'A',
+  1: 'B',
+  2: 'C',
+  3: 'D',
+  4: 'E',
+  5: 'F',
+  6: 'G',
+  7: 'H',
+  8: 'I',
+  9: 'J',
+  10: 'K',
+  11: 'L',
+  12: 'M',
+  13: 'N',
+  14: 'O',
+  15: 'P',
+  16: 'Q',
+  17: 'R',
+  18: 'S',
+  19: 'T',
+  20: 'U',
+  21: 'V',
+  22: 'W',
+  23: 'X',
+  24: 'Y',
+  25: 'Z',
+  26: 'a',
+  27: 'b',
+  28: 'c',
+  29: 'd',
+  30: 'e',
+  31: 'f',
+  32: 'g',
+  33: 'h',
+  34: 'i',
+  35: 'j',
+  36: 'k',
+  37: 'l',
+  38: 'm',
+  39: 'n',
+  40: 'o',
+  41: 'p',
+  42: 'q',
+  43: 'r',
+  44: 's',
+  45: 't',
+  46: 'u',
+  47: 'v',
+  48: 'w',
+  49: 'x',
+  50: 'y',
+  51: 'z',
+  52: '0',
+  53: '1',
+  54: '2',
+  55: '3',
+  56: '4',
+  57: '5',
+  58: '6',
+  59: '7',
+  60: '8',
+  61: '9',
+  62: '+',
+  63: '/'
+};
+
+var lib2 = {
+  'A': 0,
+  'B': 1,
+  'C': 2,
+  'D': 3,
+  'E': 4,
+  'F': 5,
+  'G': 6,
+  'H': 7,
+  'I': 8,
+  'J': 9,
+  'K': 10,
+  'L': 11,
+  'M': 12,
+  'N': 13,
+  'O': 14,
+  'P': 15,
+  'Q': 16,
+  'R': 17,
+  'S': 18,
+  'T': 19,
+  'U': 20,
+  'V': 21,
+  'W': 22,
+  'X': 23,
+  'Y': 24,
+  'Z': 25,
+  'a': 26,
+  'b': 27,
+  'c': 28,
+  'd': 29,
+  'e': 30,
+  'f': 31,
+  'g': 32,
+  'h': 33,
+  'i': 34,
+  'j': 35,
+  'k': 36,
+  'l': 37,
+  'm': 38,
+  'n': 39,
+  'o': 40,
+  'p': 41,
+  'q': 42,
+  'r': 43,
+  's': 44,
+  't': 45,
+  'u': 46,
+  'v': 47,
+  'w': 48,
+  'x': 49,
+  'y': 50,
+  'z': 51,
+  '0': 52,
+  '1': 53,
+  '2': 54,
+  '3': 55,
+  '4': 56,
+  '5': 57,
+  '6': 58,
+  '7': 59,
+  '8': 60,
+  '9': 61,
+  '+': 62,
+  '/': 63
+};
+
 String.prototype.toBase64 = function() {
-  var string = this.valueOf().split(''), 
-      output = [];
-  console.log(string);
-  string.forEach( (v, i, a) => {
-    v = v.charCodeAt(0).toString(2);
-    while(v.length < 8) {
-    v = '0'.concat(v);
+
+  function convertToBinaryOctets(arr) {
+    arr.forEach( function(v, i, a) {
+      v = v.charCodeAt(0).toString(2);
+      while(v.length < 8) {
+      v = '0'.concat(v);
+      }
+      a[i] = v;
+    });
+    return arr;
+  }
+
+  function convertOctetsToSextets(arr) {
+    var output = [];
+    arr = arr.join('').split('');
+    while(arr.length) {
+      output.push(arr.splice(0, 6).join(''));
     }
-    a[i] = v;
-  });
-  string = string.join('').split('');
-  while(string.length) {
-    output.push(string.splice(0, 6).join(''));
+    return output;
+  }
+
+  function convertSextetsToChars(arr) {
+    arr.forEach( function(v, i, a) {
+      a[i] = lib1[parseInt(v, 2)];
+    });
+    return arr.join('');
   }
   
-  output.forEach( (v, i, a) => {
-    console.log(v);
-    a[i] = String.fromCharCode(parseInt(v, 2) + 64);
-  });
+  return convertSextetsToChars( convertOctetsToSextets( convertToBinaryOctets( this.valueOf().split('') ) ) );
   
-  return output.join('');
-  
-}
+};
 
 String.prototype.fromBase64 = function() {
- var string = this.valueOf();
-}
+
+  function convertCharsToSextets(arr) {
+    arr.forEach( function(v, i, a) {
+      v = lib2[v].toString(2);
+      while(v.length < 6) {
+      v = '0'.concat(v);
+      }
+      a[i] = v;
+    });
+    return arr;
+  }
+
+  function convertSextetsToOctets(arr) {
+    var output = [];
+    arr = arr.join('').split('');
+    while(arr.length) {
+      output.push(arr.splice(0, 8).join(''));
+    }
+    return output;
+  }
+
+  function convertOctetsToChars(arr) {
+    arr.forEach( function(v, i, a) {
+      a[i] = String.fromCharCode(parseInt(v, 2));
+    });
+    return arr.join('');
+  }
+
+  return convertOctetsToChars( convertSextetsToOctets( convertCharsToSextets( this.valueOf().split('') ) ) );
+
+};
+
+console.log('Man'.toBase64());
+console.log('TWFu'.fromBase64());
+console.log('this is a string!!'.toBase64());
+console.log('dGhpcyBpcyBhIHN0cmluZyEh'.fromBase64());
