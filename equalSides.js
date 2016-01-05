1790b8150455b2f6fd6555872405e71a9e0fcab1
Garron Sanchez <garronmichael@gmail.com>
2016-01-05 10:31:19 -0800

    add solution

--- equalSides.js
+++ equalSides.js
@@ -41,3 +41,17 @@ console.log(findEvenIndex([1,2,3,4,3,2,1])); // 3
 console.log(findEvenIndex([1,100,50,-51,1,1])); // 1
 console.log(findEvenIndex([1,2,3,4,5,6])); // -1
 
+// Solution 
+
+function findEvenIndex(arr) {
+  var left = 0, right = arr.reduce(function(pv, cv) { return pv + cv; }, 0);
+  for(var i = 0; i < arr.length; i++) {
+      if(i > 0) left += arr[i-1];
+      right -= arr[i];
+      
+      if(left == right) return i;
+  }
+  
+  return -1;
+}
+
