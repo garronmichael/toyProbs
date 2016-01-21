74bc80259d0cc7ecaf14bc86d0dd4b08ede7f6f8
Garron Sanchez <garronmichael@gmail.com>
2016-01-21 15:42:13 -0800

    initial commit

---
+++ longestPalindrome.js
@@ -0,0 +1,29 @@
+/*
+Longest Palindrome
+
+Find the length of the longest substring in the given string s that is the same in reverse.
+
+As an example, if the input was “I like racecars that go fast”, the substring (racecar) length would be 7.
+
+If the length of the input string is 0, return value must be 0.
+
+Example:
+
+"a" -> 1 
+"aab" -> 2  
+"abcde" -> 1
+"zzbaabcd" -> 4
+"" -> 0
+*/
+
+longestPalindrome=function(s){
+
+}
+
+console.log(longestPalindrome("a")) // 1
+// console.log(longestPalindrome("aa")) // 2
+// console.log(longestPalindrome("baa")) // 2
+// console.log(longestPalindrome("aab")) // 2
+// console.log(longestPalindrome("zyabyz")) // 6 // "Are you sure that is a palindrome?"
+// console.log(longestPalindrome("baabcd")) // 4
+// console.log(longestPalindrome("baablkj12345432133d")) // 9
