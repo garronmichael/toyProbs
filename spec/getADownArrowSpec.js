4934e06da100ffb9045eaaf401c07e2e608825ce
Garron Sanchez <garron.michael@gmail.com>
2016-01-27 11:24:55 -0800

    create spec directory

---
+++ spec/getADownArrowSpec.js
@@ -0,0 +1,23 @@
+describe('Get A Down Arrow', function() {
+
+  it('should return something', function() {
+    expect( getADownArrow() ).toEqual( jasmine.anything() );
+  });
+
+  it('should return a string', function() {
+    expect( getADownArrow() ).toEqual( jasmine.any(String) );
+  });
+
+  it('should input 1 and return "1"', function() {
+    expect( getADownArrow(1) ).toBe("1");
+  });
+
+  it('should input 3 and return "12321\n 121\n  1"', function() {
+    expect( getADownArrow(3) ).toBe("12321\n 121\n  1");
+  });
+
+  it('should input 5 and return "123454321\n 1234321\n  12321\n   121\n    1")', function() {
+    expect( getADownArrow(1) ).toBe("123454321\n 1234321\n  12321\n   121\n    1");
+  });
+
+});
