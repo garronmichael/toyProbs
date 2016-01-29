72e900d9b6ef18868f310aec3abb96e101051c48
Garron Sanchez <garron.michael@gmail.com>
2016-01-28 17:21:37 -0800

    make compiled file for es2015 functionality

---
+++ src/smaller-compiled.js
@@ -0,0 +1,27 @@
+/*
+
+Write
+
+function smaller(arr)
+that given an array arr, you have to return the amount of numbers that are smaller than arr[i] to the right.
+
+For example:
+
+smaller([5, 4, 3, 2, 1]) === [4, 3, 2, 1, 0]
+smaller([1, 2, 0]) === [1, 1, 0]
+If you've completed this one and you feel like testing your performance tuning of this same kata, head over to the much tougher version How many are smaller than me II?
+AlgorithmsArrays
+
+*/
+
+"use strict";
+
+function smaller() {
+  var arr = arguments.length <= 0 || arguments[0] === undefined ? [] : arguments[0];
+
+  // for each value in the array
+  // for each of the following values
+  // count the number of values that are less than the current value
+  // add the count to the output array
+  return arr;
+}
