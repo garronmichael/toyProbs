b75f2db74655819180ec9528ee772408a086268e
Garron Sanchez <garron.michael@gmail.com>
2016-01-28 17:45:08 -0800

    add test for input of [5,4,3,2,1]

--- spec/smallerSpec.js
+++ spec/smallerSpec.js
@@ -4,4 +4,8 @@ describe('Smaller', function() {
     expect( smaller() ).toEqual( jasmine.any(Array) );
   });
 
+  it('should input [5, 4, 3, 2, 1] and output [4, 3, 2, 1, 0]', function() {
+    expect( smaller( [5, 4, 3, 2, 1] ).toString() ).toBe( [4, 3, 2, 1, 0].toString() );
+  });
+
 });
