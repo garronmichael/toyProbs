875a96fe926b43937d90f2bb53035797c42d84f2
Garron Sanchez <garron.michael@gmail.com>
2016-01-28 17:51:21 -0800

    add several test cases

--- spec/smallerSpec.js
+++ spec/smallerSpec.js
@@ -8,4 +8,24 @@ describe('Smaller', function() {
     expect( smaller( [5, 4, 3, 2, 1] ).toString() ).toBe( [4, 3, 2, 1, 0].toString() );
   });
 
+  it('should input [1, 2, 3] and output [0, 0, 0]', function() {
+    expect( smaller( [1, 2, 3] ).toString() ).toBe( [0, 0, 0].toString() );
+  });
+
+  it('should input [1, 2, 0] and output [1, 1, 0]', function() {
+    expect( smaller( [1, 2, 0] ).toString() ).toBe( [1, 1, 0].toString() );
+  });
+
+  it('should input [1, 2, 1] and output [0, 1, 0]', function() {
+    expect( smaller( [1, 2, 1] ).toString() ).toBe( [0, 1, 0].toString() );
+  });
+
+  it('should input [1, 1, -1, 0, 0] and output [3, 3, 0, 0, 0]', function() {
+    expect( smaller( [1, 1, -1, 0, 0] ).toString() ).toBe( [3, 3, 0, 0, 0].toString() );
+  });
+
+  it('should input [5, 4, 7, 9, 2, 4, 4, 5, 6] and output [4, 1, 5, 5, 0, 0, 0, 0, 0]', function() {
+    expect( smaller( [5, 4 ,7, 9, 2, 4, 4, 5, 6] ).toString() ).toBe( [4, 1, 5, 5, 0, 0, 0, 0, 0].toString() );
+  });
+
 });
